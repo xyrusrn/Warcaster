606d6b25a700f3b2d70bf62708b4cb8f0ba4e43c
xyrusrn <ryannieboer@me.com>
2022-12-12 21:35:49 +0100

    yeyeye

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from os import getenv
 
 from flask import Flask
-from flask import url_for
 
 app = Flask(__name__)
 
@@ -9,24 +8,10 @@ app = Flask(__name__)
 def index():
     return "Hi there"
 
-@app.route("/login/")
-def home():
-    return "Login page"
-
-@app.route("/home/")
+@app.route("/home")
 def home():
     return "Home page"
 
-@app.route("/lore/")
+@app.route("/lore")
 def lore():
     return "Lore page"
-
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
-
-with app.test_request_context():
-    print(url_for('index'))
-    print(url_for('login'))
-    print(url_for('login', next='/'))
-    print(url_for('profile', username='Ryan Nieboer'))
