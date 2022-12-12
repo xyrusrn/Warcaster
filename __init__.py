aa247bc2735972d13de15825dd5f99672884e9be
xyrusrn <ryannieboer@me.com>
2022-12-12 22:05:57 +0100

    ye

--- __init__.py
+++ __init__.py
@@ -1,29 +1,35 @@
+####################################################################
+#################        Importing packages      ###################
+####################################################################
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
-login_manager.init_app(app)
+db = SQLAlchemy()
 
-from models import User
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret-key-goes-here'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite' 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from auth import auth as auth_blueprint
+    db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
 
-app.register_blueprint(auth_blueprint)
+    login_manager.init_app(app) 
+    from models import User
+    @login_manager.user_loader
+    def load_user(user_id): 
 
-from main import main as main_blueprint
+        return User.query.get(int(user_id))
 
-app.register_blueprint(main_blueprint)
-return app
+    from auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    from main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
