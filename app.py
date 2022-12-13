3adf5e5d33d263b27688b91ab7e99d6bb5bdd896
xyrusrn <ryannieboer@me.com>
2022-12-13 11:20:36 +0100

    yes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import (Flask, request, redirect, session, render_template)
 
 app = Flask (__name__)
+app.secret_key = 'SecretKey'
+user = {"username", "abc", "password", "xyz"}
 
-@app.route("/")
+@app.route("/login")
 def login():
     return "Forced Login"
 
