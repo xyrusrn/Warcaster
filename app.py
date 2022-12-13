ce3c4b0e4611f361dea46b575b21faee82d0ce74
xyrusrn <ryannieboer@me.com>
2022-12-13 11:50:50 +0100

    yes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import (Flask, request, redirect, session, render_template, url_for)
+from flask import (Flask, request, redirect, session, render_template, url_for, flash)
+import os
 
-app = Flask (__name__)
-app.secret_key = 'SecretKey'
+app = Flask (__name__)  
 user = {"username", "abc", "password", "xyz"}
 
 @app.route("/")
@@ -16,8 +16,23 @@ def login():
         if username == user['username'] and password == user['password']:
 
             session['user'] = username
-            return redirect('/dashboard/')
+            return redirect (url_for('dashboard'))
         return "Wrong username or password"
     
     return render_template("login.html")
 
+@app.route("/dashboard/")
+def dashboard():
+    if ('user' in session and session ['user'] == user['username']):
+        return "Welcome to the dashboard!"
+    
+    return "You are not logged in."
+
+@app.route('/logout')
+def logout():
+    session.pop('user')         
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(12)  
+    app.run(debug=True)
