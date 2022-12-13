da01791396ef56f2a212586228d1df0f4dfa234d
xyrusrn <ryannieboer@me.com>
2022-12-13 22:40:42 +0100

    www

--- Website/auth.py
+++ Website/auth.py
@@ -9,6 +9,19 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in succesfully!', category='success')
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
     return render_template("login.html")
 
 @auth.route('/logout')
@@ -23,7 +36,10 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater then 3 characters.', category='error')
         elif len(first_name) < 2:
             flash('First Name must be greater then 1 characters.', category='error')
