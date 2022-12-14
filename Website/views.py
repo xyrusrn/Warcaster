a93b4350fa0feb493cf87e739ebbd305327e95ac
xyrusrn <ryannieboer@me.com>
2022-12-14 15:59:04 +0100

    wwwww

--- Website/views.py
+++ Website/views.py
@@ -1,9 +1,43 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify, render_template, flash, request, jsonify
 from flask_login import login_required, current_user
+from .models import Note
+from . import db
+import json
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET','POST'])
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+            return jsonify({})
+
+@views.route('/notes', methods=['GET','POST'])
+@login_required
+def notes():
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        if len(note) < 1:
+            flash('Note is too short!', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note has been added!', category='success')
+    return render_template("notes.html", user=current_user)
+
+@views.route('/character', methods=['GET','POST'])
+@login_required
+def character():
+    return render_template("character.html", user=current_user)
